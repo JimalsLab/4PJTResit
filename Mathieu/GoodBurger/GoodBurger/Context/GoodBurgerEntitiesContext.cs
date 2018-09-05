a05d208a85f70039ecd49820f27c85b8d316e5b1
MATHIEU JACOB <mathieu.jacob@sogeti.com>
2018-09-05 16:56:47 +0200

    a

--- Mathieu/GoodBurger/GoodBurger/Context/GoodBurgerEntitiesContext.cs
+++ Mathieu/GoodBurger/GoodBurger/Context/GoodBurgerEntitiesContext.cs
@@ -64,7 +64,6 @@ namespace GoodBurger.Context
         public string Price { get; set; }
         public string Picture { get; set; }
         public string Description { get; set; }
-
         public string Type { get; set; }
     }
 }
